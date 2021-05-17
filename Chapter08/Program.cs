b5115c03f2abb3fe1733032face315f2b7d73fb9
Nikita Galanov <galanov888@gmail.com>
2021-05-17 12:52:27 +0300

    примеры из главы 9

--- Chapter08/Program.cs
+++ Chapter08/Program.cs
@@ -15,7 +15,8 @@ namespace Chapter08
             //ExampleManyInterfaces();
             //ExampleInterfaceHierarchy();
             //ExampleIEnumerable();
-            ExampleIClonebale();
+            //ExampleIClonebale();
+            ExampleSwap();
             Console.WriteLine("Hello World!");
         }
 
@@ -188,6 +189,29 @@ namespace Chapter08
             carArray[4] = new Car("Chucky", 40, 5);
 
             Array.Sort(carArray);
+        }
+
+        static void Swap<T>(ref T a, ref T b)
+        {
+            Console.WriteLine("You sent the Swap() method a {0}", typeof(T) ) ;
+            T temp = a;
+            a = b;
+            b = temp;
+        }
+
+        static void ExampleSwap()
+        {
+            // Обмен двух целочисленных значений
+            int a = 10, b = 90;
+            Console.WriteLine("Before swap: {0}, {1}", a, b);
+            Swap<int>(ref a, ref b);
+            Console.WriteLine("After swap: {0}, {1}", a, b);
+            Console.WriteLine();
+            // Обмен двух строковых значений,
+            string si = "Hello", s2 = "There";
+            Console.WriteLine("Before swap: {0} {1}'", si, s2);
+            Swap<string>(ref si, ref s2);
+            Console.WriteLine("After swap: {0} {1}!", si, s2);
         }
     }
 }
